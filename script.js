3edcc7f6e0e8057ad921297de82ae8c15c997c48
Pastafredda <justspotify09@gmail.com>
2023-05-04 17:13:09 +0200

    mettiamo le immagini in pagina con l'array

---
+++ script.js
@@ -0,0 +1,7 @@
+const images = ["img/01.webp", "img/02.webp", "img/03.webp", "img/04.webp", "img/05.webp"];
+const items= document.getElementById("items");
+
+for (let i= 0; i < images.length; i++ ){
+    const item= `<div class= "item ${images}">${images}</div>`;
+    items.innerHTML += item 
+}
