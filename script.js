28522fd6bc3781325593483120432fa067366511
Pastafredda <justspotify09@gmail.com>
2023-05-04 18:00:23 +0200

    a

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const images = ["img/01.webp", "img/02.webp", "img/03.webp", "img/04.webp", "img/05.webp"];
-const items= document.getElementById("items");
+const items= document.querySelectorAll("items");
+let activeItem = 0;
 
 for (let i= 0; i < images.length; i++ ){
-    const item= `<div class= "item${images[i]}"><img src="${images[i]}" alt=""></div>`;
+    const item= `<div class= "item"><img src="${images[i]}" alt=""></div>`;
     items.innerHTML += item;
+    items[activeItem].classList.add('active');
+    
 }
+
