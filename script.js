76c33ed89fe61f6e4b56229ef0f5b3e45bba2e16
Pastafredda <justspotify09@gmail.com>
2023-05-04 17:36:11 +0200

    correzione

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const images = ["img/01.webp", "img/02.webp", "img/03.webp", "img/04.webp", "img
 const items= document.getElementById("items");
 
 for (let i= 0; i < images.length; i++ ){
-    const item= `<div class= "item ${images}">${images}</div>`;
-    items.innerHTML += item 
+    const item= `<div class= "item${images[i]}"><img src="${images[i]}" alt=""></div>`;
+    items.innerHTML += item;
 }
