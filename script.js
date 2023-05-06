686aba8ce96b0fedd47da76adfc68c14a2697f67
Pastafredda <justspotify09@gmail.com>
2023-05-06 13:05:12 +0200

    funzionamento dei puslanti e loop

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // Variabili
 const images = ["img/01.webp", "img/02.webp", "img/03.webp", "img/04.webp", "img/05.webp"];
 const items= document.getElementById("items");
+const previus= document.getElementById("previus");
+const next= document.getElementById("next");
+
 
 // Portiamo le immagini in pagina
 for (let i= 0; i < images.length; i++ ){
@@ -19,6 +22,49 @@ const containerItems = document.querySelectorAll("#items .item");
 containerItems[firstImage].classList.add('active');
 
 // Facciamo funzionare le frecce
+next.addEventListener('click', nextFunction)
+
+function nextFunction(){
+
+    // Rimuoviamo la classe active alla prima immagine
+    containerItems[firstImage].classList.remove('active');
+
+    // Diciamo di farlo per le successive
+    firstImage++;
+
+    // Quando la classe active arriverà all'ultimo elemento
+    if(firstImage == images.length){
+
+        // Gli diciamo di tornare a 0 per creare il loop
+        firstImage = 0;
+    }
+
+    containerItems[firstImage].classList.add('active');
+  
+}
+
+previus.addEventListener('click', previusFunction)
+
+function previusFunction(){
+    
+    // Rimuoviamo la classe active alla prima immagine
+    containerItems[firstImage].classList.remove('active');
+
+    // Diciamo di farlo per le precedenti in questo caso
+    firstImage--;
+
+    // Quando la classe active arriverà al primo elemento
+    if(firstImage < 0){
+
+        // Gli diciamo tornare all'ultima immagine
+        firstImage = images.length -1;
+    }
+
+    
+    containerItems[firstImage].classList.add('active');
+}
+
+
 
 
 
