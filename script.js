35725f1196d1a23cf8d8d012b9b7e15ab5d1cef3
Pastafredda <justspotify09@gmail.com>
2023-05-06 12:00:36 +0200

    aggiustamenti e selezione delle immagini

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
+// Variabili
 const images = ["img/01.webp", "img/02.webp", "img/03.webp", "img/04.webp", "img/05.webp"];
-const items= document.querySelectorAll("items");
-let activeItem = 0;
+const items= document.getElementById("items");
 
+// Portiamo le immagini in pagina
 for (let i= 0; i < images.length; i++ ){
-    const item= `<div class= "item"><img src="${images[i]}" alt=""></div>`;
-    items.innerHTML += item;
-    items[activeItem].classList.add('active');
     
+    // Immagini principale
+    const item = `<div class= "item"><img src="${images[i]}" alt="image"></div>`;
+    items.innerHTML += item; 
+
 }
+
+// Settiamo la posizione di partenza
+let firstImage= 0;
+
+// Selezioniamo gli elementi creati
+const containerItems = document.querySelectorAll("#items .item");
+containerItems[firstImage].classList.add('active');
+
+// Facciamo funzionare le frecce
+
+
+
+
+
 
